97d27e2e32a32db8656c9258a39dde0d17c5eac7
C0SM0S-git <Vladyslav.Voievodin.OI.2024@lpnu.ua>
2026-01-24 17:45:04 +0200

    fix torch + ultralytics compatibility

--- api.py
+++ api.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from ultralytics import YOLO
 
 app = FastAPI()
+
+model = None
+
+@app.on_event("startup")
+def load_model():
+    global model
+    model = YOLO("yolov8n-cls.pt")  # або твій .pt
+    print("YOLO loaded")
 
 @app.get("/")
 def root():
